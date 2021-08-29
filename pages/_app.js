8395f51bf5365e37e65ee226e40f68f44e95bd79
BrunoBeltreGuzman <brunodevv@gmail.com>
2021-08-29 11:39:36 -0400

    init new project

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-//import "../theme/dark/global.css";
-
-// This default export is required in a new `pages/_app.js` file.
-export default function App({ Component, pageProps }) {
-       return <Component {...pageProps} />;
-}
--- src/components/app.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function App(props) {
-       return (
-              <div>
-                     <h1>Bobo</h1>
-              </div>
-       );
-}
